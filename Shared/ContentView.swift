3c7067d161fdd8d79957de47cd5e86698332202b
shiwei93 <stayfocusjs@gmail.com>
2020-12-10 22:45:54 +0800

    add navigation view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Text("Hello world")
+                .navigationBarItems(
+                    leading: Button("11", action: {
+
+                    }),
+                    trailing: Button("22", action: {
+
+                    })
+                )
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+
+            ContentView()
+                .environment(\.colorScheme, .dark)
+        }
     }
 }
