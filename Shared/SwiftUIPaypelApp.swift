c915bffc0d2a892ab0f8cb63d03d008f20456dbb
shiwei93 <stayfocusjs@gmail.com>
2020-12-10 22:23:36 +0800

    Initial Commit

---
+++ Shared/SwiftUIPaypelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPaypelApp.swift
+//  Shared
+//
+//  Created by 施伟 on 2020/12/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPaypelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
