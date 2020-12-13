2002f55902682cde6cdae22cfa3ebd4b7ef9ad31
shiwei93 <stayfocusjs@gmail.com>
2020-12-13 17:27:44 +0800

    fix: card view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,29 +6,44 @@
 //
 
 import SwiftUI
+import SwiftUIX
 
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            Text("Hello world")
-                .toolbar {
-                    ToolbarItem(placement: .principal) {
-                        HStack {
-                            Image(systemName: "sun.min.fill")
-                            Text("Title")
-                                .font(.headline)
+            ZStack {
+                Color.blue
+
+                VStack(alignment: .leading) {
+                    Image("Avatar")
+                    Text("Nina te ha enviado")
+                    Text("20,00€")
+                    HStack {
+                        Button(action: {
+
+                        }, label: {
+                            Text("VER DETALLES")
+                                .foregroundColor(.white)
+                        })
+                        .padding()
+                        .border(Color.white, width: 1)
+                        .cornerRadius(15)
+
+                        VStack(alignment: .leading) {
+                            Text("Mensaje")
+                                .foregroundColor(Color.white)
+                            Text("Por la cena 🍕")
                         }
                     }
                 }
-                .navigationBarItems(
-                    leading: Button("11", action: {
-
-                    }),
-                    trailing: Button("22", action: {
-
-                    })
-                )
+            }
+            .navigationBarTitleView(Image("logo"))
+            .navigationBarItems(
+                leading: Text("Avatar"),
+                trailing: Text("Setting")
+            )
         }
+
     }
 }
 
